eccf7a8578c09dd9f11bb3b9eaa1f763cfa2ef2c
hedredo <lekeuj@hotmail.com>
2025-02-07 12:49:17 +0100

    Add Dataloaderv8 class and enhance data loading functionality; update session notes and dependencies

--- notebooks/archive.py
+++ notebooks/archive.py
@@ -622,3 +622,192 @@ class Dataloaderv6(tf.keras.utils.PyDataset):
                 ]
 
         return np.asarray(batch_images), np.asarray(batch_masks)
+
+
+class Dataloaderv7(tf.keras.utils.PyDataset):
+    def __init__(
+        self,
+        image_folder: pathlib.Path,
+        labels: typing.NamedTuple,
+        batch_size: int,
+        target_size: typing.Tuple[int, int],
+        preview: int = None,  # Load only a subset of the dataset for preview.
+        rgb_norm: bool = True,
+        shuffle: bool = True,
+        label_onehot: bool = False,
+        threadpool: bool = False,
+        **kwargs,
+    ):
+        """
+        Initialize the Dataloaderv6 data generator.
+        """
+        super().__init__(**kwargs)
+        self.image_folder = image_folder
+        self.labels = labels
+        self.batch_size = batch_size
+        self.target_size = target_size
+        self.preview = preview
+        self.rgb_norm = rgb_norm
+        self.shuffle = shuffle
+        self.label_onehot = label_onehot
+        self.threadpool = threadpool  # If True, use multiprocessing.Pool
+
+        # Get image and mask file paths using pathlib
+        self.image_paths = sorted(list(image_folder.glob("*leftImg8bit.png")))
+        self.mask_paths = sorted(list(image_folder.glob("*labelIds.png")))
+        if len(self.image_paths) != len(self.mask_paths):
+            self.logger.error(
+                "Number of images (%d) and masks (%d) must be equal.",
+                len(self.image_paths),
+                len(self.mask_paths),
+            )
+            raise ValueError("Number of images and masks must be equal.")
+
+        # Apply preview if requested
+        if self.preview is not None:
+            self.image_paths = self.image_paths[: self.preview]
+            self.mask_paths = self.mask_paths[: self.preview]
+
+        # Shuffle the dataset before starting
+        if self.shuffle:
+            self.on_epoch_end()
+
+        self.num_samples = len(self.image_paths)
+        self.table_id2category = {label.id: label.categoryId for label in self.labels}
+        self.num_classes = len(set(self.table_id2category.values()))
+        # Setup dedicated logger for this class
+        self.logger = logging.getLogger("dataloader")
+        self.logger.setLevel(logging.DEBUG)
+
+        # Disable PIL logging DEBUG
+        logging.getLogger("PIL").setLevel(logging.WARNING)
+
+    def __len__(self) -> int:
+        length = math.ceil(self.num_samples / self.batch_size)
+        self.logger.debug(f"Number of batches: {length}")
+        return length
+
+    def __getitem__(self, index: int):
+        start_idx = index * self.batch_size
+        end_idx = min(start_idx + self.batch_size, self.num_samples)
+        if start_idx >= self.num_samples:
+            raise IndexError("Index out of range")
+
+        batch_images_paths = self.image_paths[start_idx:end_idx]
+        batch_masks_paths = self.mask_paths[start_idx:end_idx]
+
+        self.logger.debug(
+            f"Fetching batch {index}: start_idx={start_idx}, end_idx={end_idx}"
+        )
+
+        if self.threadpool:
+            self.use_multiprocessing = False
+            with multiprocessing.Pool() as pool:
+                batch_images = pool.map(self.load_img_to_array, batch_images_paths)
+                batch_masks = pool.map(self.load_mask_to_array, batch_masks_paths)
+                if self.label_onehot:
+                    batch_masks = pool.map(
+                        self.transform_mask_to_categorical, batch_masks
+                    )
+        else:
+            batch_images = [self.load_img_to_array(path) for path in batch_images_paths]
+            batch_masks = [self.load_mask_to_array(path) for path in batch_masks_paths]
+            if self.label_onehot:
+                batch_masks = [
+                    self.transform_mask_to_categorical(mask) for mask in batch_masks
+                ]
+
+        return np.asarray(batch_images), np.asarray(batch_masks)
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            zip_paths = list(zip(self.image_paths, self.mask_paths))
+            np.random.shuffle(zip_paths)
+            self.image_paths, self.mask_paths = zip(*zip_paths)
+        else:
+            pass
+
+    def load_img_to_array(self, img_path: pathlib.Path):
+        img = tf.keras.utils.load_img(
+            str(img_path),
+            target_size=self.target_size,
+            color_mode="rgb",
+            interpolation="bilinear",
+        )
+        img_array = tf.keras.utils.img_to_array(img)
+        return img_array / 255.0 if self.rgb_norm else img_array
+
+    def load_mask_to_array(self, mask_path: pathlib.Path):
+        mask = tf.keras.utils.load_img(
+            str(mask_path),
+            target_size=self.target_size,
+            color_mode="grayscale",
+            interpolation="nearest",
+        )
+        mask_array = tf.keras.utils.img_to_array(mask)
+        # Map mask ids to categories
+        mapped = np.vectorize(self.table_id2category.get)(mask_array)
+        return mapped.squeeze()
+
+    def transform_mask_to_categorical(self, mask):
+        return tf.keras.utils.to_categorical(mask, num_classes=self.num_classes)
+
+
+# Dimensions cibles
+TARGET_HEIGHT = 256
+TARGET_WIDTH = 512
+
+def tf_map_table(labels: typing.NamedTuple):
+    # Create a StaticVocabularyTable to map ids to categories
+    table = tf.lookup.StaticVocabularyTable(
+        tf.lookup.KeyValueTensorInitializer(
+            [label.id for label in labels],
+            [label.categoryId for label in labels],
+            key_dtype=tf.int64,  # Needs to be tf.int64 or tf.string
+            value_dtype=tf.int64,  # Needs to be tf.int64 or tf.string
+        ),
+        num_oov_buckets=1,
+    )
+    return table
+
+def load_image(image_path):
+    image = tf.io.read_file(image_path)
+    image = tf.io.decode_png(image, channels=3)  # Images en couleur
+    image = tf.image.convert_image_dtype(image, tf.float32)  # Normalisation
+    image = tf.image.resize(
+        image, [TARGET_HEIGHT, TARGET_WIDTH], method="bilinear"
+    )  # Redimensionnement
+    return image
+
+
+def load_mask(mask_path, tf_map_table):
+    mask = tf.io.read_file(mask_path)
+    mask = tf.io.decode_png(
+        mask, channels=1, dtype=tf.uint8
+    )  # Annotation en niveau de gris
+    # annotation = tf.map_fn(fn= lambda x: )
+    mask = tf.image.resize(
+        mask, [TARGET_HEIGHT, TARGET_WIDTH], method="nearest"
+    )  # Redimensionnement
+    mask = tf.cast(mask, tf.int64)
+    return tf_map_table.lookup(mask)
+
+
+def load_data(image_path, mask_path):
+    image = load_image(image_path)
+    mask = load_mask(mask_path)
+    return image, mask
+
+
+def create_dataset(folder_path, batch_size=8, preview=None):
+    image_paths = sorted(list(map(str, folder_path.glob("*leftImg8bit.png"))))
+    mask_paths = sorted(list(map(str, folder_path.glob("*labelIds.png"))))
+    if preview is not None:
+        image_paths = image_paths[:preview]
+        mask_paths = mask_paths[:preview]
+    dataset = tf.data.Dataset.from_tensor_slices((image_paths, mask_paths))
+    dataset = dataset.shuffle(buffer_size=len(image_paths))
+    dataset = dataset.map(load_data, num_parallel_calls=tf.data.AUTOTUNE)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+    return dataset
